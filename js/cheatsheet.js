56e22e42f1d9f8b9b01e561247a7d37991e85680
Xeoth <xeoth@xeoth.me>
2020-06-11 00:27:59 +0200

    changed tooltip into a sidebar

---
+++ js/cheatsheet.js
@@ -0,0 +1,8 @@
+function showCheatsheet() {
+  if ($('.markdown-toolbox-cheatsheet').css('opacity') === '0') {
+    $('.markdown-toolbox-cheatsheet').css('opacity', '1');
+  } else {
+    $('.markdown-toolbox-cheatsheet').css('opacity', '0');
+  }
+
+}
