4a520bda9cd13b14a41f2a5201dd270b749bcebc
Xeoth <xeoth@xeoth.me>
2020-06-10 03:14:22 +0200

    Generating works

---
+++ src/generator.js
@@ -0,0 +1,13 @@
+function generate() {
+  const recipient = $('#recipient-text-box').val();
+  const subject = $('#subject-text-box').val();
+  const message = $('#message-textarea').val();
+
+  let fullURL = `https://reddit.com/message/compose?to=${recipient}&subject=${encodeURI(subject)}&message=${encodeURI(message)}`;
+
+  $('#result').val(fullURL);
+}
+
+function copy() {
+
+}
