8329b0929340ce9339b0f58ff2b4130002bc7460
Xeoth <xeoth@xeoth.me>
2020-06-10 23:38:23 +0200

    changed the displaying of the 'you must fill all fields' error

--- js/generator.js
+++ js/generator.js
@@ -4,12 +4,12 @@ function generate() {
   const message = $('#message-textarea');
 
   if (!recipient.val() || !subject.val() || !message.val()) {
-    $('.incomplete-fields-warning').css('visibility', 'visible');
+    $('.incomplete-fields-warning').css('display', 'block');
     [recipient, subject, message].forEach(el => el.val() ? {} : el.css('border', '2px solid red'));
     return;
   } else {
     [recipient, subject, message].forEach(el => el.val() ? el.css('border', 'none') : {});
-    $('.incomplete-fields-warning').css('visibility', 'hidden');
+    $('.incomplete-fields-warning').css('display', 'none');
   }
 
   let fullURL = `https://reddit.com/message/compose?to=${recipient.val()}&subject=${encodeURIComponent(subject.val())}&message=${encodeURIComponent(message.val())}`;
