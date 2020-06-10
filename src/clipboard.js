5be8809f37d2651a97c0b7af2d4ce422cdc0a091
Xeoth <xeoth@xeoth.me>
2020-06-10 18:38:42 +0200

    added confirmation of copy

--- src/clipboard.js
+++ src/clipboard.js
@@ -7,3 +7,10 @@ clipboard.on('success', function (e) {
 clipboard.on('error', function (e) {
   console.error(e);
 });
+
+function changeButtonText() {
+  $('.button-text').html('Copied!');
+  setTimeout(function () {
+    $('.button-text').html('Copy');
+  }, 1000);
+}
