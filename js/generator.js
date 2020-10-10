feda7636cd25095f26b7f5423dad9e6e1e2a2ef1
Xeoth <46075322+Xeoth@users.noreply.github.com>
2020-10-10 23:19:03 +0200

    changed functions to jquery

--- js/generator.js
+++ js/generator.js
@@ -1,24 +1,17 @@
-function generate() {
-  const recipient = $('#recipient-text-box').val();
-  const subject = $('#subject-text-box').val();
-  const message = $('#message-textarea').val();
+$('#generate-btn').click(() => {
+  const recipient = $('#recipientInput').val();
+  const subject = $('#titleInput').val();
+  const message = $('#messageContent').val();
 
   if (!recipient || !subject || !message) return;
 
-  let fullURL = `https://reddit.com/message/compose?to=${recipient.val()}&subject=${encodeURIComponent(subject.val())}&message=${encodeURIComponent(message.val())}`;
+  let fullURL = `https://reddit.com/message/compose?to=${recipient}&subject=${encodeURIComponent(subject)}&message=${encodeURIComponent(message)}`;
 
   console.log(fullURL)
-}
+})
 
-function clear() {
-  console.log('lol');
-  $('#recipient-text-box').val('');
-  $('#subject-text-box').val('');
-  $('#message-textarea').val('');
-
-}
-
-$('#generate-btn').click((e) => {
-  e.preventDefault()
-  console.log('yeet');
+$('#clear-btn').click(() => {
+  $('#recipientInput').val('');
+  $('#titleInput').val('');
+  $('#messageContent').val('');
 })
