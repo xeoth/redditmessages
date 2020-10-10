0e04960d22103ab7ce186c0cffc8d489ba394e28
Xeoth <46075322+Xeoth@users.noreply.github.com>
2020-10-10 20:11:33 +0200

    overall redesign and shift to bootstrap

--- js/generator.js
+++ js/generator.js
@@ -1,18 +1,24 @@
 function generate() {
-  const recipient = $('#recipient-text-box');
-  const subject = $('#subject-text-box');
-  const message = $('#message-textarea');
+  const recipient = $('#recipient-text-box').val();
+  const subject = $('#subject-text-box').val();
+  const message = $('#message-textarea').val();
 
-  if (!recipient.val() || !subject.val() || !message.val()) {
-    $('.incomplete-fields-warning').css('display', 'block');
-    [recipient, subject, message].forEach(el => el.val() ? {} : el.css('border', '2px solid red'));
-    return;
-  } else {
-    [recipient, subject, message].forEach(el => el.val() ? el.css('border', 'none') : {});
-    $('.incomplete-fields-warning').css('display', 'none');
-  }
+  if (!recipient || !subject || !message) return;
 
   let fullURL = `https://reddit.com/message/compose?to=${recipient.val()}&subject=${encodeURIComponent(subject.val())}&message=${encodeURIComponent(message.val())}`;
 
-  $('#result').val(fullURL);
+  console.log(fullURL)
 }
+
+function clear() {
+  console.log('lol');
+  $('#recipient-text-box').val('');
+  $('#subject-text-box').val('');
+  $('#message-textarea').val('');
+
+}
+
+$('#generate-btn').click((e) => {
+  e.preventDefault()
+  console.log('yeet');
+})
