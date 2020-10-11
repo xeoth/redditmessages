68ad0d2d9333058131e570eb8681a29e938649bf
Xeoth <46075322+Xeoth@users.noreply.github.com>
2020-10-11 17:52:27 +0200

    added the copy button

--- js/clipboard.js
+++ js/clipboard.js
@@ -1,12 +1,1 @@
-const clipboard = new ClipboardJS('.copy-button');
-
-clipboard.on('success', function (e) {
-  $('.button-text').html('Copied!');
-  setTimeout(function () {
-    $('.button-text').html('Copy');
-  }, 1000);
-});
-
-clipboard.on('error', function (e) {
-  console.error(e);
-});
+const clipboard = new ClipboardJS('#copy-btn');
