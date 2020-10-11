acecd6e6529bd55abced4ba82954e3a729425fcd
Xeoth <46075322+Xeoth@users.noreply.github.com>
2020-10-11 18:19:17 +0200

    button now has a nice animation on copying

--- js/clipboard.js
+++ js/clipboard.js
@@ -1,1 +1,13 @@
 const clipboard = new ClipboardJS('#copy-btn');
+
+clipboard.on('success', e => {
+  const $copyButton = $('#copy-btn');
+
+  $copyButton.removeClass('btn-outline-primary').addClass('btn-outline-success');
+  $copyButton.html('Copied!');
+
+  setTimeout(() => {
+    $copyButton.removeClass('btn-outline-success').addClass('btn-outline-primary');
+    $copyButton.html('<img src="./icons/clipboard.svg" alt="clipboard icon"> Copy');
+  }, 1000);
+})
